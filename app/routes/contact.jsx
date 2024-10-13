e680d16a86995fb30c859e8afad8be60ef390de6
Charles Stello <cstello@protonmail.com>
2024-10-13 12:26:43 -0400

    added animations to the contact page

--- app/routes/contact.jsx
+++ app/routes/contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {useRef} from "react";
 
 // Components
 import NavBar from "../Components/NavBar/navBar";
@@ -7,6 +7,7 @@ import "./Styles/contact.scss";
 
 // Utils
 import { json } from "@remix-run/react";
+import { m as motion, useInView } from "framer-motion";
 
 export const meta = () => {
     return [
@@ -87,57 +88,78 @@ export const loader = async ({request}) => {
 };
 
 export default function Contact() {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true, margin: "0px 0px -200px 0px"});
+    const contactIsInView = useInView(ref, { once: true, margin: "0px 0px -200px 0px"});
+
+    const containerVariants = {
+        hidden: { opacity: 1, scale: 0 },
+        visible: {
+            opacity: 1,
+            scale: 1,
+            transition: {
+                delayChildren: 0.3,
+                staggerChildren: 0.2
+            }
+        }
+    };
+    
+    const cardVariants = {
+        hidden: { opacity: 0, y: 20 },
+        visible: { opacity: 1, y: 0, transition: { duration: 0.6, ease: "easeInOut" } },
+    };
+
     return (
-        <div className="contactPage">
+        <div className="contactPage" ref={ref}>
             <NavBar/>
 
             <div className="header">
-                <div className="text">
+                <motion.div className="text" initial={{ opacity: 0, x: -100 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5 }}>
                     <div className="title">
                         <h1>Get in Touch<span>.</span></h1>
                     </div>
                     <p className="byline">Think I could help you out? Reach out through the form and I'll get back to you as soon as I can! I'm looking forward to meeting you!</p>
                     
-                    <form className="form" action="https://formspree.io/f/xrgvrjdp" method="post">
-                        <input type="text" name="name" placeholder="name" required/>
-                        <input type="email" name="email" placeholder="email" required/>
-                        <textarea name="message" placeholder="message" required/>
-                        <button className="submitBtn">Send <i className="bx bx-paper-plane"/></button>
-                    </form>
-                </div>
+                    <motion.form className="form" action="https://formspree.io/f/xrgvrjdp" method="post" variants={containerVariants} initial="hidden" animate={isInView ? "visible" : "hidden"}>
+                        <motion.input variants={cardVariants} type="text" name="name" placeholder="name" required/>
+                        <motion.input variants={cardVariants} type="email" name="email" placeholder="email" required/>
+                        <motion.textarea variants={cardVariants} name="message" placeholder="message" required/>
+                        <motion.button variants={cardVariants} className="submitBtn">Send <i className="bx bx-paper-plane"/></motion.button>
+                    </motion.form>
+                </motion.div>
 
-                <div className="photo">
+                <motion.div className="photo" initial={{ opacity: 0, x: 100 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.5 }}>
                     <div className="wrapper">
                         <img src="/sakura.webp" alt="SyntaxisCS Profile Picture"/>
                     </div>
-                </div>
+                </motion.div>
             </div>
 
             <div className="alsoCheck">
                 <h2>You can also find me on<span>:</span></h2>
 
-                <div className="socials">
-                    <a className="social" href="https://gitlab.com/SyntaxisCS" target="_blank" aria-label="SyntaxisCS Gitlab">
+                <motion.div className="socials" variants={containerVariants} initial="hidden" animate={contactIsInView ? "visible" : "hidden"}>
+                    <motion.a variants={cardVariants} className="social" href="https://gitlab.com/SyntaxisCS" target="_blank" aria-label="SyntaxisCS Gitlab">
                         <div className="iconWrap">
                             <i className="bx bxl-gitlab"/>
                         </div>
                         <p>SyntaxisCS</p>
-                    </a>
+                    </motion.a>
 
-                    <a className="social" href="https://github.com/SyntaxisCS" target="_blank" aria-label="SyntaxisCS Github">
+                    <motion.a className="social" variants={cardVariants} href="https://github.com/SyntaxisCS" target="_blank" aria-label="SyntaxisCS Github">
                         <div className="iconWrap">
                             <i className="bx bxl-github"/>
                         </div>
                         <p>SyntaxisCS</p>
-                    </a>
+                    </motion.a>
 
-                    <a className="social" href="https://www.linkedin.com/in/charlesstello/" target="_blank" aria-label="Charles Stello LinkedIn">
+                    <motion.a className="social" variants={cardVariants} href="https://www.linkedin.com/in/charlesstello/" target="_blank" aria-label="Charles Stello LinkedIn">
                         <div className="iconWrap">
                             <i className="bx bxl-linkedin"/>
                         </div>
                         <p>CharlesStello</p>
-                    </a>
-                </div>
+                    </motion.a>
+                </motion.div>
             </div>
 
             <Footer/>
